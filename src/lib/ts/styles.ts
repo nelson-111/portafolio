914a3206d5652f62373d6adf4e54a3394fcb9896
Jackdanelrub <jackdanelrubhasher@gmail.com>
2022-10-01 20:36:29 -0500

    .

--- src/lib/ts/styles.ts
+++ src/lib/ts/styles.ts
@@ -8,7 +8,7 @@ function randoms_sum(max_value: number, number_length: number){
 
   while(index <= number_length)
   {
-    result += random(max_value) / number_length
+    result += random(max_value / number_length)
   }
 
   return result
