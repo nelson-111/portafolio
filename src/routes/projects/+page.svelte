9986e87ba155b93a0ad83654bbdfb9d4d47ce3fa
Jackdanelrub <jackdanelrubhasher@gmail.com>
2022-11-20 12:20:42 -0500

    process.env added

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,11 +3,14 @@
 <script lang="ts" defer>
   import {github_icon} from '$lib/ts/icons'
 
+  let token = process.env.NODE_ENV === 'production'
+  ? process.env.VITE_GITHUB_API_TOKEN : import.meta.env.VITE_GITHUB_API_TOKEN
+
   let projects = fetch('https://api.github.com/users/nelson-111/repos',
   {
     method: 'GET',
     headers: {
-      'Authorization': `token ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      'Authorization': `token ${token}`,
     }
   })
   .then(result => result.json())
