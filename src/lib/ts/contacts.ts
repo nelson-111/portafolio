0265d22791709e2c9be7c2921474478903914f1d
null <85085896+null010101@users.noreply.github.com>
2023-05-21 10:49:02 -0500

    Delete src directory

--- src/lib/ts/contacts.ts
+++
@@ -1,9 +0,0 @@
-import { github_icon, stack_overflow_icon, reddit_icon } from './icons';
-
-export const contacts = [
-  {
-    alt: 'Github',
-    href: 'https://github.com/nelson-111',
-    icon: github_icon
-  },
-]
